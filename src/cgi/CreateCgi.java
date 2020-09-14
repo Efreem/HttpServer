78aa1946c13ef16c1ef5156e52a78355d347f568
Efreem <truecybersec@gmail.com>
2020-09-14 14:04:06 +0600

    Created CreateCgi.java

---
+++ src/cgi/CreateCgi.java
@@ -0,0 +1,46 @@
+package cgi;
+
+import java.util.Map;
+
+public class CreateCgi {
+    private String readycgi;
+    public static String httpheader = "HTTP/1.0 ";
+    public static Map<String,String> code_status = Map.of(
+            "200", "200 OK\n",
+            "204", "204 No Response\n",
+            "404", "404 Not Found\n");
+    public static String content_type = "Content-Type: text/html\n";
+    public static String content_length = "Content-Length: ";
+
+    public CreateCgi() {
+        readycgi = "";
+    }
+
+    public String getReadyCgi() {
+        return readycgi;
+    }
+
+    public CreateCgi addHttpHeader() {
+        readycgi += CreateCgi.httpheader;
+        return this;
+    }
+
+    public CreateCgi addCodeStatus(String code_status) {
+        readycgi += CreateCgi.code_status.get(code_status);
+        return this;
+    }
+
+    public CreateCgi addContentType() {
+        readycgi += CreateCgi.content_type;
+        return this;
+    }
+
+    public CreateCgi addContentLength(Integer Content_Length) {
+        readycgi += CreateCgi.content_length + Content_Length.toString() + "\n\n";
+        return this;
+    }
+
+    public void addContent(String content) {
+        readycgi += content;
+    }
+}
